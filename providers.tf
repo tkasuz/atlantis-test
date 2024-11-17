e35fa675759d34b9814be58b8aff6fd75085c158
tkasuz <63289889+tkasuz@users.noreply.github.com>
2024-11-17 23:15:09 +0900

    host.docker.internal

--- providers.tf
+++ providers.tf
@@ -10,8 +10,8 @@ terraform {
     region                      = "us-east-1"
     use_path_style              = true
     endpoints = {
-      s3  = "http://localhost:4566"
-      sts = "http://localhost:4566"
+      s3  = "http://host.docker.internal:4566"
+      sts = "http://host.docker.internal:4566"
     }
   }
   required_providers {
@@ -33,25 +33,24 @@ provider "aws" {
 
 
   endpoints {
-    apigateway     = "http://localhost:4566"
-    cloudformation = "http://localhost:4566"
-    cloudwatch     = "http://localhost:4566"
-    dynamodb       = "http://localhost:4566"
-    es             = "http://localhost:4566"
-    firehose       = "http://localhost:4566"
-    iam            = "http://localhost:4566"
-    kinesis        = "http://localhost:4566"
-    lambda         = "http://localhost:4566"
-    route53        = "http://localhost:4566"
-    redshift       = "http://localhost:4566"
-    s3             = "http://localhost:4566"
-    secretsmanager = "http://localhost:4566"
-    ses            = "http://localhost:4566"
-    sns            = "http://localhost:4566"
-    sqs            = "http://localhost:4566"
-    ssm            = "http://localhost:4566"
-    stepfunctions  = "http://localhost:4566"
-    sts            = "http://localhost:4566"
+    apigateway     = "http://host.docker.internal:4566"
+    cloudformation = "http://host.docker.internal:4566"
+    cloudwatch     = "http://host.docker.internal:4566"
+    dynamodb       = "http://host.docker.internal:4566"
+    es             = "http://host.docker.internal:4566"
+    firehose       = "http://host.docker.internal:4566"
+    iam            = "http://host.docker.internal:4566"
+    kinesis        = "http://host.docker.internal:4566"
+    lambda         = "http://host.docker.internal:4566"
+    route53        = "http://host.docker.internal:4566"
+    redshift       = "http://host.docker.internal:4566"
+    s3             = "http://host.docker.internal:4566"
+    secretsmanager = "http://host.docker.internal:4566"
+    ses            = "http://host.docker.internal:4566"
+    sns            = "http://host.docker.internal:4566"
+    sqs            = "http://host.docker.internal:4566"
+    ssm            = "http://host.docker.internal:4566"
+    stepfunctions  = "http://host.docker.internal:4566"
+    sts            = "http://host.docker.internal:4566"
   }
-
 }
