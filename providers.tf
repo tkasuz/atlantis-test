8fbd3d592c449ee99086c537430a570ea4b0a9c7
tkasuz <63289889+tkasuz@users.noreply.github.com>
2024-11-17 12:37:05 +0900

    chore: state in s3

--- providers.tf
+++ providers.tf
@@ -1,5 +1,12 @@
 terraform {
-  backend "local" {}
+  backend "s3" {
+    bucket           = "terraform-state"
+    key              = "terraform.tfstate"
+    region           = "us-east-1"
+    endpoint         = "http://localhost:4566"
+    sts_endpoint     = "http://localhost:4566"
+    force_path_style = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
